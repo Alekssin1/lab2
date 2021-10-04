3063847cdf6dfaabbfe94c9c17adbe8917549ae4
Viacheslav Moskalenko <xomkaxomiakovuch@gmail.com>
2021-10-04 14:39:08 +0300

    update1.0

--- Rectangle.py
+++ Rectangle.py
@@ -3,7 +3,7 @@ class Rectangle:
     width = 1
 
     def set_length_and_width(self, length, width):
-        if not isinstance(length, (int, float)) or not isinstance(width, (int, float)):
+        if not isinstance(length, float) or not isinstance(width, float):
             raise TypeError("U must enter a number")
         if (0 < length < 20) and (0 < width < 20):
             self.length = length
@@ -25,6 +25,19 @@ class Rectangle:
 
 
 my_rectangle = Rectangle()
-my_rectangle.set_length_and_width(14.5, 13)
-print(my_rectangle.perimeter_count())
-print(my_rectangle.area_count())
+user_rectangle = Rectangle()
+my_rectangle.set_length_and_width(14.5, 13.3)
+print("My rectangle perimeter: " + str(my_rectangle.perimeter_count()) +
+      "\nMy rectangle area: " + str(my_rectangle.area_count()))
+
+try:
+    length_inp = float(input('Enter length: '))
+    width_inp = float(input('Enter width: '))
+except ValueError or TypeError or EOFError:
+    print('Incorrect input u should write a number')
+    exit()
+user_rectangle.set_length_and_width(length_inp, width_inp)
+print(
+    "My rectangle perimeter: " + str(user_rectangle.perimeter_count()) +
+    "\nMy rectangle area: " + str(user_rectangle.area_count())
+)
